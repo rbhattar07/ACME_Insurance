db7c5cacc9b416c95512a02f2893cca2bfccf26c
Riddh Bhattar <riddhbhattar@gmail.com>
2023-02-28 11:57:52 -0600

    EDA File

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ urlretrieve(url, 'medical.csv')
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import matplotlib
 import seaborn as sns
 import plotly.express as px
@@ -16,4 +16,7 @@ df = pd.read_csv('medical.csv')
 smoker_values = {'no':0, 'yes':1}
 df['smoker_numeric'] = df.smoker.map(smoker_values)
 print(df)
+
+# Creating the basic understanding of our Model in terms of model layout
+# Charges = w * age * b
 
